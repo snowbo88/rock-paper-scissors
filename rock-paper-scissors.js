d4dd822b86e2a4f26bf0da3c20df23533660f493
Bo <boyadeng@gmail.com>
2022-06-26 02:27:36 -0400

    JS code works well.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,17 @@
-// game();
-// playerSelection();
-playRound();
+let playerScore = 0;
+let computerScore = 0;
+
+const playerSelections = document.querySelectorAll('.handShapes');
+playerSelections.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection = button.id;
+        computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection)
+        keepScore();
+        displayResults(playerSelection, computerSelection, '#handShapes');
+        displayResults(playerScore, computerScore, '#score');
+    })
+})
 
 // computer selects a hand
 function computerPlay() {
@@ -10,49 +21,79 @@ function computerPlay() {
     return computerSelection;
 }
 
-function playerSelection() {
-    const playerSelections = document.querySelectorAll('button');
-    playerSelections.forEach((button) => {
-        button.addEventListener('click', () => {
-            playerSelection = button.id;
-
-            const result = document.querySelector('#results');
-            result.textContent = 'You picked ' + playerSelection;
-
-            console.log(playerSelection);
-            return playerSelection.toLowerCase();
-        })
-    })
-}
-
 // plays a round with a computer
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection()
-
-    console.log("You: " + playerSelection + ", Computer: " + computerSelection)
+    // console.log("You: " + playerSelection + ", Computer: " + computerSelection)
 
     //You draw if
     if (playerSelection === computerSelection) {
-        console.log("You draw!");
+        textContent("#round", "You draw this round!")
     }
 
     // You win if
     else if (playerSelection === "rock" && computerSelection === "scissors"
         || playerSelection === "paper" && computerSelection === "rock"
         || playerSelection === "scissors" && computerSelection === "paper") {
-        console.log("You win!");
+        playerScore += 1;
+        textContent("#round", "You win this round!")
     }
 
     //Else you win
     else {
-        console.log("You lose!")
+        computerScore += 1;
+        textContent("#round", "You lose this round!")
     }
 }
 
-// plays five rounds with computer
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//         const computerSelection = computerPlay();
-//         playRound(playerSelection(), computerSelection);
-//     }
-// }
+
+
+function displayResults(playerSelection, computerSelection, selector) {
+    const result = document.querySelector(selector);
+    result.textContent = 'You: ' + playerSelection + ', Computer: ' + computerSelection;
+}
+
+function keepScore() {
+    const result = document.querySelector("#result")
+
+    if (playerScore == 5) {
+        result.textContent = "You win!"
+        resetGame();
+    }
+
+    if (computerScore == 5) {
+        result.textContent = "You lose!"
+        resetGame();
+    }
+}
+
+function resetGame() {
+    const buttons = document.querySelectorAll(".handShapes");
+    buttons.forEach((button) => button.disabled = true)
+
+    const container = document.querySelector('#container');
+
+    const resetButton = document.createElement('button');
+    resetButton.setAttribute('type', 'button');
+    resetButton.setAttribute('id', 'resetButton');
+    resetButton.textContent = "Reset Game"
+
+    container.appendChild(resetButton);
+
+    resetButton.addEventListener('click', () => {
+        playerScore = 0;
+        computerScore = 0;
+        buttons.forEach((button) => button.disabled = false)
+
+        textContent("#result", "");
+
+        displayResults(playerSelection, computerSelection, '#handShapes');
+        displayResults(playerScore, computerScore, '#score');
+
+        container.removeChild(resetButton)
+    })
+}
+
+function textContent(selector, text) {
+    const element = document.querySelector(selector);
+    result.textContent = text;
+}
